936627a8df2ec3cbbbe705efef5efd5dd62b10fa
alex <alexandermorrison16@gmail.com>
2024-12-10 16:19:55 +1000

    added character and centred their position

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image();
 image.src = "./maps/mainOutdoor.png";
 
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
+
+const scaleFactor = 1.5;
+
 image.onload = () => {
-  c.drawImage(image, 0, 0);
+  c.drawImage(image, 0, -800);
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4,
+    canvas.height / 2 - playerImage.height / 4,
+    (playerImage.width / 4) * scaleFactor,
+    playerImage.height * scaleFactor
+  );
 };
